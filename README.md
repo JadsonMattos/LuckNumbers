f09c3ce9ab25fd3eef9af3fe2d1e996efa36f0fb
JadsonMattos <98970595+JadsonMattos@users.noreply.github.com>
2023-06-30 21:42:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LuckNumbers
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-49kyeh)
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Greeting from './components/Greeting';
+import LuckyNumbers from './components/LuckyNumbers';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Greeting />
+      <LuckyNumbers />
+    </>
+  );
+}
+
+export default App;
